817e1185b9c10a7bc7d26e1e8cf6b8e0083808f4
kadriansyah <kadriansayah@gmail.com>
2017-03-15 15:48:51 +0700

    cleanup APICache

--- lib/moslemcorners/api_cache.rb
+++ lib/moslemcorners/api_cache.rb
@@ -3,9 +3,10 @@ require_dependency 'fileutils'
 module MoslemCorners
     module APICache
         def create_cache
-            # remove slash at beginning of uri "/admin/users" becomes "admin/users"
-            logger.info("#{self.request.original_fullpath[1..-1]} -- #{self.response.body}")
+            # # remove slash at beginning of uri "/admin/users" becomes "admin/users"
+            # logger.info("#{self.request.original_fullpath[1..-1]} -- #{self.response.body}")
 
+            # remove slash at beginning of uri "/admin/users" becomes "admin/users"
             strings = self.request.original_fullpath[1..-1].split('/')
             dirs = strings[0..strings.length - 2].join('/')
             path = "#{Rails.application.config.api_cache_directory}/#{dirs}"
