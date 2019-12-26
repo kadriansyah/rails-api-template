e0d0e249570fffc003c47cd471fdbd7fbc6df9a0
Kiagus Arief Adrinsyah <kadriansyah@gmail.com>
2019-12-26 20:08:58 +0700

    cleanup run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-#docker run -d --rm --name api "$PWD":/var/www/html/#appname.com -p 80:80 -p 3000:3000 kadriansyah/#appname
-
-# link to other container
-docker run -d --name api -v "$PWD":/var/www/html/#appname.com --link mongoapi -p 80:80 -p 3000:3000 kadriansyah/#appname
+docker run -d --name api -v "$PWD":/var/www/html/#appname.com --link mongo -p 80:8080 -p 443:8443 -p 3000:3000 kadriansyah/#appname
